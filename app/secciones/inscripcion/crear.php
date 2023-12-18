055351546b9f9dea9996e28752b6677cce4bcc09
KevinEscobedo <kevinescobedo248@gmail.com>
2023-12-18 14:53:33 -0300

    Agregado calendario con clases disponibles en creacion de inscripciones

--- app/secciones/inscripcion/crear.php
+++ app/secciones/inscripcion/crear.php
@@ -5,12 +5,62 @@ date_default_timezone_set('America/Argentina/Buenos_Aires');
 
 
 $hoy = date("Y-m-d H:i:s");
-$sentencia2 = $conexion->prepare("SELECT * FROM clase where fch_desde > :hoy ");
+$sentencia2 = $conexion->prepare("SELECT cl.*, COUNT(ins.id_inscripcion) as inscripciones FROM `clase` as cl LEFT join inscripcion as ins on ins.id_clase = cl.id_clase WHERE cl.fch_desde > :hoy GROUP BY cl.id_clase order by cl.fch_desde ");
 $sentencia2->bindParam(":hoy", $hoy);
 $sentencia2->execute();
 $lista_clases = $sentencia2->fetchAll(PDO::FETCH_ASSOC);
 
 
+
+class Event
+{
+    public $title;
+    public $start;
+    public $end;
+    public $color;
+    // public $id;
+    public $textColor;
+
+    // public function __construct($nombre, $precio) {
+    //     $this->nombre = $nombre;
+    //     $this->precio = $precio;
+    // }
+}
+
+$lista_eventos = array();
+foreach ($lista_clases as $indice => $clase) {
+    $newEvent = new Event();
+    $inscripcionesRestantes = $clase["cant_max_participantes"] - $clase["inscripciones"];
+
+    if ($inscripcionesRestantes <= 0) {
+        $newEvent->color = "Red";
+    } elseif ($inscripcionesRestantes <= 5) {
+        $newEvent->color = "Yellow";
+        $newEvent->textColor = "Black";
+    } else {
+        $newEvent->color = "Green";
+    }
+    $newEvent->title = $clase["descripcion"] . "-Disponibles: " . $inscripcionesRestantes . "/" . $clase["cant_max_participantes"];
+    // Crear un objeto DateTime desde la cadena original
+    $fechaObjeto = DateTime::createFromFormat('Y-m-d H:i:s', $clase["fch_desde"]);
+
+    // Obtener la fecha en el formato deseado
+    $fechaFormateada = $fechaObjeto->format('Y-m-d\TH:i');
+    $newEvent->start = $fechaFormateada;
+
+    $fechaObjeto = DateTime::createFromFormat('Y-m-d H:i:s', $clase["fch_hasta"]);
+
+    // Obtener la fecha en el formato deseado
+    $fechaFormateada = $fechaObjeto->format('Y-m-d\TH:i');
+    $newEvent->end = $fechaFormateada;
+
+    // events: [{
+    //     title: 'Clase nocturna',
+    //     start: '2023-18-12T01:30',
+    //     allDay: false
+    // }],
+    array_push($lista_eventos, $newEvent);
+}
 if ($_POST) {
     if (!empty($_POST)) {
         $mes = date_create('-31 day')->format('Y-m-d H:i:s');
@@ -81,21 +131,49 @@ if ($_POST) {
 ?>
 
 <?php include("../../templates/header.php"); ?>
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.css">
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/locales-all.js"></script>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            locale: "es",
+            headerToolbar: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            },
+
+            events: <?php echo json_encode($lista_eventos) ?>,
+
+        });
+        calendar.render();
+    });
+</script>
+
 <div class="card mt-3 mb-3">
     <div class="card-header bg-secondary text-white">
-        Datos del Abono
+        Nueva Inscripcion
     </div>
     <div class="card-body bg-dark">
+
         <form action="" method="post" enctype="multipart/form-data">
 
 
+            <div class="container-calendar" style="background-color:white">
+                <div class="col-md-8 offset-md-2">
+                    <div id='calendar'></div>
+                </div>
+            </div>
 
-
+            </body>
             <div class="mb-3">
-                <label for="id_clase" class="form-label bg-dark text-white">Tipo abono</label>
+                <label for="id_clase" class="form-label bg-dark text-white">Seleccione la clase a anotarse</label>
                 <select class="form-select bg-dark text-white" name="id_clase" id="id_clase">
                     <?php foreach ($lista_clases as $clase) { ?>
-                        <option value="<?php echo $clase['id_clase'] ?>"><?php echo $clase['descripcion'] ?> </option>
+                        <option value="<?php echo $clase['id_clase'] ?>"><?php echo  $clase['descripcion'] . " " . "(" . $clase['fch_desde'] . " a " . $clase['fch_hasta'] . ")" ?> </option>
                     <?php }
                     ?>
                 </select>
